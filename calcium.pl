c99f9c84871d990ab72a766d58f73c1debea58b6
Derek Hoagland <thegrickit@gmail.com>
2011-08-14 15:11:53 -0400

    It now actually compiles the page! I just have to print it into files now.

--- calcium.pl
+++ calcium.pl
@@ -1,8 +1,6 @@
 use strict;
 use warnings;
 use FindBin;
-use lib $FindBin::Bin . '/modules/';
-use caldoc;
 use File::Path;
 use Cwd;
 
@@ -24,6 +22,72 @@ sub stopGoing {
   exit;
 }
 
+sub getDocumentVariable {
+  return $documents->{$_[0]}->{$_[1]};
+}
+
+sub preCompile {
+  my $self = {};
+  $self->{'id'} = shift;
+  open (my $fileHandle, $self->{'id'}.'.cal');
+  $self->{'baseContents'} = join("", <$fileHandle>);
+  $self->{'unparsedContents'} = '';
+  close $fileHandle;
+
+  foreach my $current_line (split(/[\r\n]+/,$self->{'baseContents'})) {
+    $current_line =~ s/^[\s\t]+//;
+    if ($current_line =~ /^<cal include="[a-z0-9_.-]+">$/i) {
+      $self->{'unparsedContents'} .= "$current_line\n";
+    }
+    elsif ($current_line =~ /^<cal print="[a-z0-9_.-]+">$/i) {
+      $self->{'unparsedContents'} .= "$current_line\n";
+    }
+    elsif ($current_line =~ /^<cal ([a-z0-9_-]+)="([a-z0-9_.\/\\-]+)">$/i) {
+      $self->{$1} = $2;
+    }
+    else {
+      $self->{'unparsedContents'} .= "$current_line\n";
+    }
+  }
+  return $self;
+}
+
+sub pageCompile {
+  my $self = shift;
+
+  $self->{'Contents'} = '';
+  $documents->{'parent'} = $self;
+  foreach my $current_line (split(/[\r\n]+/,$self->{'unparsedContents'})) {
+    $current_line =~ s/^[\s\t]+//;
+    if ($current_line =~ /^<cal include="([a-z0-9_.-]+)">$/i) {
+      print "Including $1\n";
+      $self->{'Contents'} .= resourceCompile($documents->{$1})."\n";
+    }
+    else {
+      $self->{'Contents'} .= "$current_line\n";
+    }
+  }
+  delete $documents->{'parent'};
+  return $self;
+}
+
+sub resourceCompile {
+  my $self = shift;
+
+  $self->{'Contents'} = '';
+  foreach my $current_line (split(/[\r\n]+/,$self->{'unparsedContents'})) {
+    $current_line =~ s/^[\s\t]+//;
+    if ($current_line =~ /^<cal print="([a-z0-9_-]+)\.([a-z0-9_-]+)">$/i) {
+      print "Printing $1.$2\n";
+      $self->{'Contents'} .= getDocumentVariable($1,$2)."\n";
+    }
+    else {
+      $self->{'Contents'} .= "$current_line\n";
+    }
+  }
+  return $self->{'Contents'};
+}
+
 sub parseCommand {
   if ($_[0] =~ /^dir ([a-z0-9_.-]+)$/i) {
     dirCommand($1);
@@ -40,7 +104,12 @@ sub parseCommand {
 }
 
 sub compileCommand {
-
+  while (my ($key, $value) = each %{$documents}) {
+    if ($documents->{$key}->{'location'}) {
+      $documents->{$key} = pageCompile($value);
+      print $documents->{$key}->{'Contents'};
+    }
+  }
 }
 
 sub dirCommand {
@@ -48,8 +117,9 @@ sub dirCommand {
 }
 
 sub loadCommand {
-  $documents->{$_[0]} = caldoc->precompile($guideFileDirectory.$_[0]);
+  $documents->{$_[0]} = preCompile($guideFileDirectory.$_[0]);
 }
+
 
 foreach my $current_line (split(/[\r\n]+/,$guideFile)) {
   print "$current_line\n";
